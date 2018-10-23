73806ca79bfb73b86e5ff20682b19ef036293052
Tom Lam <tomlam0828@gmail.com>
2018-10-22 18:48:08 -0700

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,79 @@
-$(document).ready(function() {
-  var random = Math.floor(Math.random()*101+19)
+$(document).ready(function () {
+    var random = Math.floor(Math.random() * 101 + 1);
 
-  $("#display-number").text(random);
+    var num1 = Math.floor(Math.random() * 10 + 1);
+    var num2 = Math.floor(Math.random() * 10 + 1);
+    var num3 = Math.floor(Math.random() * 10 + 1);
+    var num4 = Math.floor(Math.random() * 10 + 1);
 
-  var num1 = Math.floor(Math.random()*11+1);
-  var num2 = Math.floor(Math.random()*11+1);
-  var num3 = Math.floor(Math.random()*11+1);
-  var num4 = Math.floor(Math.random()*11+1);
+    var wins = 0;
+    var losses = 0;
+    var userTotal = 0;
 
-  var userTotal = 0;
-  var wins = 0;
-  var losses = 0;
-
-  $("#user-wins").text(wins);
-  $("#user-losses").text(losses);
-
-  function reset() {
-    var random = Math.floor(Math.random()*101+19);
+    $("#user-wins").text(wins);
+    $("#user-losses").text(losses);
     $("#display-number").text(random);
 
-    num1 = Math.floor(Math.random()*11+1);
-    num2 = Math.floor(Math.random()*11+1);
-    num3 = Math.floor(Math.random()*11+1);
-    num4 = Math.floor(Math.random()*11+1);
-    userTotal = 0;
-    $("#total-score").text(wins);
-    // reset();
-  }
-
-  function yay() {
-      alert("Yeah you won the game!");
-      wins++;
-      $("#user-wins").text(wins);
-      reset();
-  }
-
-  function loser() {
-      alert("Oh no, you lost, please try again!");
-      losses++;
-      $("#user-losses").text(losses);
-      reset()
-  }
-
-  $("#ruby").on("click", function() {
-      userTotal = userTotal + num1;
-      $("#total-score").text(userTotal);
-      if (userTotal === random) {
-          yay();
-      } else if (userTotal > random) {
-         loser();
-      }
-
-  })
-
-  $("#diamond").on("click", function() {
-      userTotal = userTotal + num2;
-      $("#total-score").text(userTotal);
-      if (userTotal === random) {
-        yay();
-    } else if (userTotal > random) {
-       loser();
+    function yes() {
+        alert("Oh Yeah you won");
+        wins++;
+        $("#user-wins").text(wins);
+        reset();
     }
-  })
 
-  $("#yellow-stone").on("click", function() {
-      userTotal = userTotal + num3;
-      $("#total-score").text(userTotal);
-      if (userTotal === random) {
-        yay();
-    } else if (userTotal > random) {
-       loser();
+    function lose() {
+        alert("Oh no you lost!");
+        losses++;
+        $("#user-losses").text(losses);
+        reset();
     }
-  })
 
-  $("#emerald").on("click", function() {
-      userTotal = userTotal + num4;
-      $("#total-score").text(userTotal);
-      if (userTotal === random) {
-        yay();
-    } else if (userTotal > random) {
-       loser();
+    function reset() {
+        random = Math.floor(Math.random() * 101 + 1);
+        num1 = Math.floor(Math.random() * 10 + 1);
+        num2 = Math.floor(Math.random() * 10 + 1);
+        num3 = Math.floor(Math.random() * 10 + 1);
+        num4 = Math.floor(Math.random() * 10 + 1);
+        userTotal = 0;
+        $("#display-number").text(random);
+        $("#total-score").text(userTotal);
+        
     }
-  });
-});
+
+    $("#ruby").on("click", function () {
+        userTotal = userTotal + num1;
+        $("#total-score").text(userTotal);
+        if (userTotal === random) {
+            yes();
+        } else if (userTotal > random) {
+            lose();
+        }
+    })
+    $("#diamond").on("click", function () {
+        userTotal = userTotal + num2;
+        $("#total-score").text(userTotal);
+        if (userTotal === random) {
+            yes();
+        } else if (userTotal > random) {
+            lose();
+        }
+    })
+    $("#yellow-stone").on("click", function () {
+        userTotal = userTotal + num3;
+        $("#total-score").text(userTotal);
+        if (userTotal === random) {
+            yes();
+        } else if (userTotal > random) {
+            lose();
+        }
+    })
+    $("#emerald").on("click", function () {
+        userTotal = userTotal + num4;
+        $("#total-score").text(userTotal);
+        if (userTotal === random) {
+            yes();
+        } else if (userTotal > random) {
+            lose();
+        }
+    })
+    })
